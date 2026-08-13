1b21ce13e8557ab2f713e5bd7ebecf85304ec42f
MaxPower296 <felix.wieser@web.de>
2026-08-13 19:10:28 +0200

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["tesseract.js", "sharp", "exifr"],
+  serverExternalPackages: ["sharp", "exifr", "tesseract.js", "tesseract.js-core"],
+  turbopack: {},
 };
 
 export default nextConfig;
